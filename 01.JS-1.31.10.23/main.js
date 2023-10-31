64bd4b10ae7f901a1da2a144b968babcfd01f3ee
NezrinMuxtarova <nezrinmuxtarova13@gmail.com>
2023-11-01 01:26:17 +0400

    js-1

---
+++ 01.JS-1.31.10.23/main.js
@@ -0,0 +1,89 @@
+//TASK1
+
+// let rightTriangle= "katet1=3, katet2=4 ,hipotenuz=5";
+// if ( rightTriangle ==="katet1=3", "katet2=4", "hipotenuz=5"){
+//     console.log("Duzbucaqli ucbucaqdir")
+// }else if(rightTriangle === "katet1=3", "katet2=3", "hipotenuz=8" ){
+//     console.log("Duzbucaqli ucbucaq deyil")
+// }else{
+//     console.log("Ucbucaq deyil")
+// }
+
+//TASK1
+// let rightTriangle= a=7, b=4, c=5;
+// if(rightTriangle=   a*2+b*2==c*2   ){
+//     console.log("Duzbucaqli ucbucaqdir")
+// }else{
+//     console.log("Duzbucaqli ucbucaq deyil")
+// }
+
+//TASK2
+
+// let number;
+// number="abc";
+// if(number="abc"){
+//     console.log("a, b, c")
+// }
+
+//Task 4
+let month = "January";
+switch (month) {
+  case  "January" :
+   console.log("January has 31 days") ;
+    break;
+  case "February":
+   console.log( "February has 28 days");
+
+    break;
+  case "March":
+    console.log("March has 31 days") ;
+
+    break;
+  case "April":
+   console.log("April has 30 days") ;
+
+    break;
+  case "May":
+    console.log("May has 31 days") ;
+
+    break;
+  case "June":
+    console.log(  "June has 30 days");
+
+    break;
+  case "July":
+    console.log("July has 31 days");
+
+    break;
+  case "August":
+    console.log("August has 31 days");
+
+    break;
+  case "September":
+    console.log("September has 30 days")  ;
+
+    break;
+  case "October":
+    console.log ("October has 31 days") ;
+
+    break;
+  case "November":
+    console.log( "November has 30 days");
+
+    break;
+  case "December":
+    console.log("December has 31 days") ;
+
+    break;
+
+  default:
+    break;
+}
+
+//TASK 5
+let number=11;
+singular=a1,a3,a5,a7,a9;
+a1,a3,a5,a7,a9=A;
+if(number=singular){
+ console.log("A tek reqemdir")
+}
