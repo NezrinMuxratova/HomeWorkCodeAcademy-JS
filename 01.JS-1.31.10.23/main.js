1ad17b7bc89ebc1155eb7b27b4a2ea531c6961f3
NezrinMuxtarova <nezrinmuxtarova13@gmail.com>
2023-11-01 01:51:28 +0400

    js-1

--- 01.JS-1.31.10.23/main.js
+++ 01.JS-1.31.10.23/main.js
@@ -17,73 +17,72 @@
 //     console.log("Duzbucaqli ucbucaq deyil")
 // }
 
-//TASK2
+// TASK2
 
-// let number;
-// number="abc";
-// if(number="abc"){
-//     console.log("a, b, c")
-// }
+let number =652;
+let modulus=10;
+  console.log(65%35, 347%30)
+
 
 //Task 4
-let month = "January";
-switch (month) {
-  case  "January" :
-   console.log("January has 31 days") ;
-    break;
-  case "February":
-   console.log( "February has 28 days");
+// let month = "January";
+// switch (month) {
+//   case  "January" :
+//    console.log("January has 31 days") ;
+//     break;
+//   case "February":
+//    console.log( "February has 28 days");
 
-    break;
-  case "March":
-    console.log("March has 31 days") ;
+//     break;
+//   case "March":
+//     console.log("March has 31 days") ;
 
-    break;
-  case "April":
-   console.log("April has 30 days") ;
+//     break;
+//   case "April":
+//    console.log("April has 30 days") ;
 
-    break;
-  case "May":
-    console.log("May has 31 days") ;
+//     break;
+//   case "May":
+//     console.log("May has 31 days") ;
 
-    break;
-  case "June":
-    console.log(  "June has 30 days");
+//     break;
+//   case "June":
+//     console.log(  "June has 30 days");
 
-    break;
-  case "July":
-    console.log("July has 31 days");
+//     break;
+//   case "July":
+//     console.log("July has 31 days");
 
-    break;
-  case "August":
-    console.log("August has 31 days");
+//     break;
+//   case "August":
+//     console.log("August has 31 days");
 
-    break;
-  case "September":
-    console.log("September has 30 days")  ;
+//     break;
+//   case "September":
+//     console.log("September has 30 days")  ;
 
-    break;
-  case "October":
-    console.log ("October has 31 days") ;
+//     break;
+//   case "October":
+//     console.log ("October has 31 days") ;
 
-    break;
-  case "November":
-    console.log( "November has 30 days");
+//     break;
+//   case "November":
+//     console.log( "November has 30 days");
 
-    break;
-  case "December":
-    console.log("December has 31 days") ;
+//     break;
+//   case "December":
+//     console.log("December has 31 days") ;
 
-    break;
+//     break;
 
-  default:
-    break;
-}
+//   default:
+//     break;
+// }
 
-//TASK 5
-let number=11;
-singular=a1,a3,a5,a7,a9;
-a1,a3,a5,a7,a9=A;
-if(number=singular){
- console.log("A tek reqemdir")
-}
+// //TASK 5
+// let numbers=11;
+// singular=a1,a3,a5,a7,a9;
+// a1,a3,a5,a7,a9=A;
+// if(number=singular){
+//  console.log("A tek reqemdir")
+// }
