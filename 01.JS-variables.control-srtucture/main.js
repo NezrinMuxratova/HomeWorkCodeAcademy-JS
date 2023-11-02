79fa721d5452bace2ef022b2d17a62ab322920a8
NezrinMuxtarova <nezrinmuxtarova13@gmail.com>
2023-11-02 00:44:50 -0700

    JS-2

--- 01.JS-variables.control-srtucture/main.js
+++ 01.JS-variables.control-srtucture/main.js
@@ -1,6 +1,5 @@
 // Verilmiş 3 rəqəmli ədədin rəqəmlərinin cəmini, hasilini və ədədi ortasını tapan proqram tərtib edin.console.log(Reqemlerin cemi: ${sum}, hasili: ${multiply}, ededi ortasi: ${average});
 
-
 //Task1-1
 // let a;
 // let first=a%10;
@@ -8,18 +7,17 @@
 // let third=((a-first)/10 -second)/10;
 // let multiply=first*second *third;
 // let averahe=sum/3;
-//Task1-2
-// function divisors (numbers){
-//     let divisors;
-//     for (let i = 1; i<= numbers; i++) {
-//        if(numbers%i===0){
-//         divisors.push(i)
-//        }
-//        return divisors.length>1 ? divisors: `${numbers}`
-//        }
-//         console.log(divisors(30));
-       
+// Task1 - 2;
+// function divisors(numbers) {
+//   let divisors;
+//   for (let i = 1; i <= numbers; i++) {
+//     if (numbers % i === 0) {
+//       divisors.push(i);
 //     }
+//     return divisors.length > 1 ? divisors : `${numbers}`;
+//   }
+//   console.log(divisors(30));
+// }
 //2
 
 // let a = 30;
@@ -61,21 +59,50 @@
 // }
 
 //task6 0-100 arasinda yalniz sade ededleri cap edin
-
-
-// let number = 100;
-// for (let index = 0; index < 100; index++) {
-//   if (number % index === 0) console.log(index);
+// let isPrime = true;
+// let primeNumber = 7;
+// for (let i = 2; i < primeNumber; i++) {
+//   if (primeNumber % i === 0) {
+//     isPrime = false;
+//     break;
+//   }
+// }
+// if (isPrime == true) {
+//   console.log(`${primeNumber} is prime`);
+// } else  {
+//   console.log(`${primeNumber} is not prime`);
+// }
+// let bool=true;
+// for (let i = 2; i <= 100; i++) {
+//   for (let j = 2; j < i; j++) {
+//     if (i % j === 0) {
+//       // console.log(i);
+//       bool=false;
+//       break;
+//     }
+//   }
+//   if(bool) console.log(i);
+//     bool=true;
+  
 // }
 
-
 //task 7  tek ededler ve cut ededleri topla
-// let sum=0;
-//  let a;
-
+// let oddSum=0;
+//  let evenSum=0;
 //  for (let i= 1; i <=100; i++) {
-//    if(a%2==0 + a%2==1){
-//       console.log ()
-//    }
+//    if((i%2)===0){
 
+//     oddSum+=i;
+
+//    }
 //  }
+//   console.log (`${oddSum} cut ededlerin cemi`)
+
+//   for (let i = 1; i <=100; i++) {
+//     if((i%2)===1){
+//         evenSum+=i
+
+//     }
+
+//   }
+//   console.log (`${evenSum} tek ededlerin cemi`)
