0417c337b15c0324636a76e366c089c806a13641
NezrinMuxtarova <nezrinmuxtarova13@gmail.com>
2023-11-02 06:18:40 +0400

    JS-1

--- 02.JS-01.11.23/main.js
+++ 02.JS-01.11.23/main.js
@@ -1,4 +1,46 @@
-// task 1
+// Verilmiş 3 rəqəmli ədədin rəqəmlərinin cəmini, hasilini və ədədi ortasını tapan proqram tərtib edin.console.log(Reqemlerin cemi: ${sum}, hasili: ${multiply}, ededi ortasi: ${average});
+
+
+//Task1-1
+// let a;
+// let first=a%10;
+// let second =((a-first)/10)%10;
+// let third=((a-first)/10 -second)/10;
+// let multiply=first*second *third;
+// let averahe=sum/3;
+//Task1-2
+// function divisors (numbers){
+//     let divisors;
+//     for (let i = 1; i<= numbers; i++) {
+//        if(numbers%i===0){
+//         divisors.push(i)
+//        }
+//        return divisors.length>1 ? divisors: `${numbers}`
+//        }
+//         console.log(divisors(30));
+       
+//     }
+//2
+
+// let a = 30;
+// for (let i = 0; i <=a; i++) {
+//   if (a % i === 0) {
+//     console.log(i);
+//   }
+// }
+
+// 3)Verilmiş ədədin bölənlərinin sayını tapan proqram tərtib edin.
+
+// let number = 10;
+// let count = 0;
+// for (let i = 0; i <= 10; i++) {
+//   if (number % i == 0) {
+//     count++;
+//   }
+// }
+// console.log(${count});
+
+// task 4
 
 // for (let i = 0; i <=10; i++) {
 //   let result = i * i
@@ -7,9 +49,7 @@
 // for (let i = 0; i <= 10; i++) {
 //    console.log (`${i} * ${i}= ${i*i}`)
 // } ikinci usul
-// task 2
-
-
+// task 5
 
 // let main = `i  i^2  i^3`;
 
@@ -20,28 +60,22 @@
 //    console.log ( `${i}   ${powerTwo}   ${powerThree}`);
 // }
 
+//task6 0-100 arasinda yalniz sade ededleri cap edin
 
-//task3 0-100 arasinda yalniz sade ededleri cap edin
 
-// let k;
-// while (k<100) {
-//    k++;
-//    if(  ){
-
-//    }
+// let number = 100;
+// for (let index = 0; index < 100; index++) {
+//   if (number % index === 0) console.log(index);
 // }
 
 
+//task 7  tek ededler ve cut ededleri topla
+// let sum=0;
+//  let a;
 
+//  for (let i= 1; i <=100; i++) {
+//    if(a%2==0 + a%2==1){
+//       console.log ()
+//    }
 
-
-//task 4. tek ededler ve cut ededleri topla
-let sum=0;
- let a;
-
- for (let i= 1; i <=100; i++) {
-   if(a%2==0 + a%2==1){
-      console.log ()
-   }
-   
- }
+//  }
