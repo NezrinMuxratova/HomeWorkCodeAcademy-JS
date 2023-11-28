8e71305f03d09339e91a761dac1ce9b6e346522f
NezrinMuxtarova <nezrinmuxtarova13@gmail.com>
2023-11-27 23:14:15 -0800

    practise

--- 07 events header/main.js
+++ 07 events header/main.js
@@ -1,29 +1,36 @@
-window.addEventListener("DOMContentLoaded", function() {
-    console.log("DOMContentLoaded event worked");
-    
-})
-window.addEventListener("unload", function() {
-    console.log("unload event worked");
-    
-})
-let header=document.querySelector("header")
+let menuIcon = document.querySelector(".fa-bars");
+let menuIcon1 = document.querySelector("#fa-xmark");
+let nav = document.querySelector("#nav");
+
+// window.addEventListener("DOMContentLoaded", function() {
+//     console.log("DOMContentLoaded event worked");
+
+// })
+// window.addEventListener("unload", function() {
+//     console.log("unload event worked");
+
+// })
+// let header=document.querySelector("header")
+// // window.addEventListener("scroll", function(){
+// //     // console.log(window.scrollY);
+// // window.scrollY > 0 ? header.classList.add("header-scroll"):
+// // header.classList.remove("header-scroll")
+// // })
+
 // window.addEventListener("scroll", function(){
-//     // console.log(window.scrollY);
-// window.scrollY > 0 ? header.classList.add("header-scroll"):
-// header.classList.remove("header-scroll")
+//     header.classList.toggle("header-scroll", window.scrollY > 0)
+//       console.log(window.scrollY);
 // })
 
-window.addEventListener("scroll", function(){
-    header.classList.toggle("header-scroll", window.scrollY > 0)
-      console.log(window.scrollY);
-})
+// menuIcon.onclick=function(){
+//     menu.classList.toggle("open")
+//     let Isopen=menu.classList.contains("open")
+//     Isopen ? menuIcon1.classList("fa-xmark") : menuIcon1.classList("fa-bars")
+// }
 
-let menu=document.querySelector("#menu")
-let menuIcon=document.querySelector(".fa-bars")
-let menuIcon1=document.querySelector(".fa-xmark")
-
-menuIcon.onclick=function(){
-    menu.classList.toggle("open")
-    let Isopen=menu.classList.contains("open")
-    Isopen ? menuIcon1.classList("fa-xmark") : menuIcon1.classList("fa-bars")
-}
+menuIcon.addEventListener("click", function () {
+  nav.classList.toggle("show");
+});
+this.classList.contains("fa-bars")
+  ? (this.classList =( "fa-solid fa-xmark"))
+  : (this.classList = ("fa-solid fa-bars"));
